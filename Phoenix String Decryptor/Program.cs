7c9d79e9bbed29eb1ad21b36ef043bdf33deb3aa
Jomtek <jomtek360@yahoo.fr>
2020-06-12 23:22:27 +0200

    Initial commit

---
+++ Phoenix String Decryptor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Phoenix_String_Decryptor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                string modulePath = "";
+                string methodToken = "";
+
+                try
+                {
+                    modulePath = args[0];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Error : no module selected... press ENTER to exit.");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+
+                Console.Write("Method token : ");
+                methodToken = Console.ReadLine().ToUpper();
+
+                if (methodToken.StartsWith("0X"))
+                    methodToken = methodToken.Substring(2);
+
+                if (methodToken.Length != 8)
+                {
+                    Console.WriteLine("Invalid method token...");
+                    Thread.Sleep(700);
+                    continue;
+                }
+
+                StringDecryptor myDecryptor = new StringDecryptor(modulePath, methodToken);
+
+                myDecryptor.OpenModule();
+
+                int totalDecryptedStrings = myDecryptor.DecryptStrings();
+                Console.WriteLine("Decrypted " + totalDecryptedStrings + " strings successfully.");
+
+                myDecryptor.SaveModule();
+
+                break;
+            }
+
+            Thread.Sleep(650);
+        }
+    }
+}
---
+++ Phoenix String Decryptor/StringDecryptor.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Pdb;
+using dnlib.DotNet.Writer;
+using Microsoft.SqlServer.Server;
+
+namespace Phoenix_String_Decryptor
+{
+    class StringDecryptor
+    {
+        private ModuleDefMD module;
+        private string modulePath;
+        private string methodToken;
+
+        public StringDecryptor(string modulePath, string methodToken)
+        {
+            this.modulePath = modulePath;
+            this.methodToken = methodToken;
+        }
+
+        private bool IsTypeAnalyzable(TypeDef moduleType) =>
+            moduleType.HasMethods && !moduleType.Namespace.Contains(".My");
+
+        private string PhoenixAlgorithm(string inputStr)
+        {
+            int length = inputStr.Length;
+            char[] array = new char[length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                char c = inputStr[i];
+                byte b = (byte)((int)c ^ length - i);
+                byte b2 = (byte)((int)(c >> 8) ^ i);
+                array[i] = (char)((int)b2 << 8 | (int)b);
+            }
+            return string.Intern(new string(array));
+        }
+
+        public void OpenModule()
+        {
+            ModuleContext modCtx = ModuleDef.CreateModuleContext();
+            module = ModuleDefMD.Load(modulePath, modCtx);
+        }
+
+        private int AnalyzeMethodBody(MethodDef method)
+        {
+            if (method.Body == null) return 0;
+
+            int decryptedStrings = 0;
+            int counter = 0;
+
+            foreach (Instruction instruction in method.Body.Instructions)
+            {
+                if (instruction.OpCode == OpCodes.Call)
+                {
+                    try
+                    {
+                        MethodDef calledMethod = (MethodDef)instruction.Operand;
+                        if (calledMethod.MDToken.ToString() == methodToken)
+                        {
+                            instruction.OpCode = OpCodes.Nop;
+
+                            Instruction ldstrInstr = method.Body.Instructions[counter - 1];
+                            ldstrInstr.Operand = PhoenixAlgorithm((string)ldstrInstr.Operand);
+
+                            decryptedStrings++;
+                        }
+                    }
+                    catch (InvalidCastException) { };
+                }
+
+                counter++;
+            }
+
+            return decryptedStrings;
+        }
+        public int DecryptStrings()
+        {
+            if (module != null)
+            {
+                int totalDecryptedStrings = 0;
+
+                for (int i = 0; i < module.Types.Count; i++)
+                {
+                    TypeDef moduleType = module.Types[i];
+                    if (IsTypeAnalyzable(moduleType))
+                    {
+                        for (int i2 = 0; i2 < moduleType.Methods.Count; i2++)
+                        {
+                            MethodDef method = moduleType.Methods[i2];
+                            totalDecryptedStrings += AnalyzeMethodBody(method);
+                            if (method.MDToken.ToString() == methodToken) moduleType.Methods.Remove(method);
+                        }
+                    }
+                }
+
+                return totalDecryptedStrings;
+            } else
+            {
+                throw new NullReferenceException();
+            }
+        }
+
+        public void SaveModule()
+        {
+            if (module != null)
+            {
+                string savePath = Path.ChangeExtension(modulePath, null);
+                var opts = new ModuleWriterOptions(module);
+                opts.MetadataOptions.Flags = MetadataFlags.KeepOldMaxStack;
+                opts.MetadataLogger = DummyLogger.NoThrowInstance;
+                module.Write(savePath + "_decrypted.exe", opts);
+            } else
+            {
+                throw new NullReferenceException();
+            }
+        }
+
+    }
+}
